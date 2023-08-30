bb77be4e77390f1981f061b333497322251104c9
g-sawada <118000212+g-sawada@users.noreply.github.com>
2023-08-30 18:13:08 +0000

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "Hello World! This is Gota!"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  # root "hello#index"  これはテンプレートのデフォルト
+  root "application#hello"
 end
